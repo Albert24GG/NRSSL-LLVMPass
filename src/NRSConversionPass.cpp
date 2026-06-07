94ac5edaf80d514aa6a6e1f8803f91b1c7ac34de
Albert24GG <guiman.albert09@gmail.com>
2026-06-07 13:41:47 +0300

    Add lowering for all systems & most instructions

--- src/NRSConversionPass.cpp
+++ src/NRSConversionPass.cpp
@@ -1,5 +1,4 @@
 #include <cstdint>
-#include <cstdlib>
 #include <iostream>
 #include <jni.h>
 #include <llvm/IR/Constants.h>
@@ -18,16 +17,130 @@
 
 using namespace llvm;
 
-static cl::opt<NRSSL::Type> selectedNRSSLType(
+namespace {
+
+cl::opt<NRSSL::Type> selected_nrs(
     "float_type", cl::desc("Choose the floating point representation"),
+    cl::init(NRSSL::Type::POSIT),
     cl::values(clEnumValN(NRSSL::Type::POSIT, "posit", "Posit representation"),
                clEnumValN(NRSSL::Type::MORRIS, "morris", "Morris representation"),
-               clEnumValN(NRSSL::Type::MORRIS_HEB, "morris_heb", "Morris HEB representation"),
-               clEnumValN(NRSSL::Type::MORRIS_BIAS_HEB, "morris_bias_heb",
-                          "Morris bias HEB representation"),
-               clEnumValN(NRSSL::Type::MORRIS_UNARY_HEB, "morris_unary_heb",
-                          "Morris unary HEB representation")),
-    cl::init(NRSSL::Type::POSIT));
+               clEnumValN(NRSSL::Type::MORRIS_HEB, "morrisHeb", "Morris HEB representation"),
+               clEnumValN(NRSSL::Type::MORRIS_UNARY_HEB, "morrisUnaryHeb",
+                          "Morris unary HEB representation"),
+               clEnumValN(NRSSL::Type::MORRIS_BIAS_HEB, "morrisBiasHeb",
+                          "Morris bias HEB representation")));
+
+class NRSIntrinsicSelector {
+  public:
+    explicit NRSIntrinsicSelector(NRSSL::Type nrs, Module &M) {
+
+        static const std::unordered_map<NRSSL::Type, std::string> nrs_names = {
+            {NRSSL::Type::POSIT, "posit"},
+            {NRSSL::Type::MORRIS, "morris"},
+            {NRSSL::Type::MORRIS_HEB, "morrisHeb"},
+            {NRSSL::Type::MORRIS_BIAS_HEB, "morrisBiasHeb"},
+            {NRSSL::Type::MORRIS_UNARY_HEB, "morrisUnaryHeb"},
+        };
+
+        const auto intrinsic_prefix = "llvm.riscv.nrssl.f32." + nrs_names.at(nrs) + ".";
+
+        // Map binary/unary ops to their corresponding intrinsics
+        {
+            static const std::array<std::pair<const char *, std::vector<unsigned>>, 6> binop_names{
+                std::make_pair("add", std::vector<unsigned>{Instruction::FAdd}),
+                std::make_pair("sub", std::vector<unsigned>{Instruction::FSub}),
+                std::make_pair("mul", std::vector<unsigned>{Instruction::FMul}),
+                std::make_pair("div", std::vector<unsigned>{Instruction::FDiv}),
+                std::make_pair("cvtToInt",
+                               std::vector<unsigned>{Instruction::FPToSI, Instruction::FPToUI}),
+                std::make_pair("cvtFromInt",
+                               std::vector<unsigned>{Instruction::SIToFP, Instruction::UIToFP})};
+
+            for (const auto &[op_name, op_opcodes] : binop_names) {
+                const auto intrinsic_name = intrinsic_prefix + op_name;
+                const auto intrinsic_id = Function::lookupIntrinsicID(intrinsic_name);
+                std::cout << "Looking up intrinsic: " << intrinsic_name << std::endl;
+                std::cout << "Intrinsic id: " << intrinsic_id << " for " << intrinsic_name
+                          << std::endl;
+                const auto intrinsic_func = Intrinsic::getDeclaration(&M, intrinsic_id);
+                if (intrinsic_func) {
+                    for (const auto op_opcode : op_opcodes) {
+                        op_to_intrinsic_[op_opcode] = intrinsic_func;
+                    }
+                    std::cout << "Mapped binary operator " << op_name << " to intrinsic function "
+                              << intrinsic_func->getName().str() << "\n";
+                } else {
+                    std::cerr << "Warning: Intrinsic " << intrinsic_name
+                              << " not found in LLVM. This operation will not be converted.\n";
+                }
+            }
+        }
+
+        // Map fcmp ops to their corresponding intrinsics
+        {
+            static constexpr std::array fcmp_names{std::make_pair("eq", FCmpOpcode::FCMP_EQ),
+                                                   std::make_pair("lt", FCmpOpcode::FCMP_LT),
+                                                   std::make_pair("le", FCmpOpcode::FCMP_LE)};
+
+            for (const auto &[fcmp_name, fcmp_opcode] : fcmp_names) {
+                const auto intrinsic_name = intrinsic_prefix + fcmp_name;
+                const auto intrinsic_id = Function::lookupIntrinsicID(intrinsic_name);
+                const auto intrinsic_func = Intrinsic::getDeclaration(&M, intrinsic_id);
+                if (intrinsic_func) {
+                    fcmp_to_intrinsic_[fcmp_opcode] = intrinsic_func;
+                    std::cout << "Mapped fcmp predicate " << fcmp_name << " to intrinsic function "
+                              << intrinsic_func->getName().str() << "\n";
+                } else {
+                    std::cerr << "Warning: Intrinsic " << intrinsic_name
+                              << " not found in LLVM. This operation will not be converted.\n";
+                }
+            }
+        }
+    }
+
+    Function *get_intrinsic_replacement(const Instruction &I) const {
+        if (I.isBinaryOp() || isa<CastInst>(I)) {
+            auto it = op_to_intrinsic_.find(I.getOpcode());
+            if (it != op_to_intrinsic_.end()) {
+                return it->second;
+            }
+        } else if (const auto *FCmp = dyn_cast<FCmpInst>(&I)) {
+            FCmpOpcode fcmp_opcode;
+            switch (FCmp->getPredicate()) {
+            case FCmpInst::FCMP_OEQ:
+            case FCmpInst::FCMP_UEQ:
+                fcmp_opcode = FCmpOpcode::FCMP_EQ;
+                break;
+            case FCmpInst::FCMP_OLT:
+            case FCmpInst::FCMP_ULT:
+                fcmp_opcode = FCmpOpcode::FCMP_LT;
+                break;
+            case FCmpInst::FCMP_OLE:
+            case FCmpInst::FCMP_ULE:
+                fcmp_opcode = FCmpOpcode::FCMP_LE;
+                break;
+            default:
+                fcmp_opcode = FCmpOpcode::FCMP_INVALID;
+                break;
+            }
+            auto it = fcmp_to_intrinsic_.find(fcmp_opcode);
+            if (it != fcmp_to_intrinsic_.end()) {
+                return it->second;
+            }
+        }
+        return nullptr; // No replacement found
+    }
+
+  private:
+    NRSIntrinsicSelector(const NRSIntrinsicSelector &) = delete;
+    NRSIntrinsicSelector &operator=(const NRSIntrinsicSelector &) = delete;
+
+    std::unordered_map<unsigned, Function *> op_to_intrinsic_{};
+    enum class FCmpOpcode { FCMP_EQ, FCMP_LT, FCMP_LE, FCMP_INVALID };
+    std::unordered_map<FCmpOpcode, Function *> fcmp_to_intrinsic_{};
+};
+
+} // namespace
 
 class NRSConversionPass : public PassInfoMixin<NRSConversionPass> {
 
@@ -56,33 +169,16 @@ class NRSConversionPass : public PassInfoMixin<NRSConversionPass> {
             }
         }
 
-        LLVMContext &ctx = M.getContext();
-        Function *nrssl_fadd_func = Intrinsic::getDeclaration(&M, Intrinsic::posit_f32_add);
+        NRSIntrinsicSelector intrinsic_selector(selected_nrs, M);
 
         for (auto &F : M) {
             for (auto &BB : F) {
-                for (auto &I : BB) {
+                for (auto it = BB.begin(); it != BB.end();) {
+                    Instruction &I = *it;
+                    // Advance the iterator before potential erases
+                    ++it;
 
-                    if (I.isBinaryOp()) {
-                        auto *binop = static_cast<BinaryOperator *>(&I);
-                        switch (binop->getOpcode()) {
-                        case Instruction::FAdd: {
-                            IRBuilder builder(&I);
-                            Value *lhs = binop->getOperand(0);
-                            Value *rhs = binop->getOperand(1);
-
-                            Value *nrssl_fadd =
-                                builder.CreateCall(FunctionCallee(nrssl_fadd_func), {lhs, rhs});
-                            binop->replaceAllUsesWith(nrssl_fadd);
-                            binop->eraseFromParent();
-
-                            break;
-                        }
-
-                        default:
-                            break;
-                        }
-                    }
+                    std::cout << "Processing instruction: " << I.getOpcodeName() << "\n";
                     if (auto *Store = dyn_cast<StoreInst>(&I)) {
                         Value *Value = Store->getValueOperand();
                         if (auto *Const = dyn_cast<Constant>(Value)) {
@@ -92,6 +188,18 @@ class NRSConversionPass : public PassInfoMixin<NRSConversionPass> {
                                 Modified = true;
                             }
                         }
+                    } else if (auto *intrinsic_replacement =
+                                   intrinsic_selector.get_intrinsic_replacement(I)) {
+                        IRBuilder builder(&I);
+                        std::vector<Value *> operands = [&I] {
+                            const auto operands = I.operands();
+                            std::vector<Value *> operand_vec(operands.begin(), operands.end());
+                            return operand_vec;
+                        }();
+                        Value *new_call = builder.CreateCall(intrinsic_replacement, operands);
+                        I.replaceAllUsesWith(new_call);
+                        I.eraseFromParent();
+                        Modified = true;
                     }
                 }
             }
@@ -113,8 +221,8 @@ class NRSConversionPass : public PassInfoMixin<NRSConversionPass> {
             float IeeeValue;
             memcpy(&IeeeValue, OldBits.getRawData(), sizeof(float));
 
-            uint32_t PositValue = nrssl.convertDoubleToUint<uint32_t>(IeeeValue, selectedNRSSLType);
-            APInt NewBits(32, PositValue);
+            uint32_t new_value = nrssl.convertDoubleToUint<uint32_t>(IeeeValue, selected_nrs);
+            APInt NewBits(32, new_value);
 
             APFloat NewAPF(APFloat::IEEEsingle(), NewBits);
             return ConstantFP::get(Context, NewAPF);
@@ -124,8 +232,8 @@ class NRSConversionPass : public PassInfoMixin<NRSConversionPass> {
             double IeeeValue;
             memcpy(&IeeeValue, OldBits.getRawData(), sizeof(double));
 
-            uint64_t PositValue = nrssl.convertDoubleToUint<uint64_t>(IeeeValue, selectedNRSSLType);
-            APInt NewBits(64, PositValue);
+            uint64_t new_value = nrssl.convertDoubleToUint<uint64_t>(IeeeValue, selected_nrs);
+            APInt NewBits(64, new_value);
 
             APFloat NewAPF(APFloat::IEEEdouble(), NewBits);
             return ConstantFP::get(Context, NewAPF);
